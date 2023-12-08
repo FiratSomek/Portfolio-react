6494d8256f106276b7dd63582457b529b4e83af9
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-12-08 15:23:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,53 @@ import { Introduce } from "../src/components/Introduce";
 import { About } from "../src/components/About";
 import { Skills } from "../src/components/Skills";
 import { MyProjects } from "../src/components/MyProjects";
+import { Contact } from "../src/components/Contact";
+import { Footer } from "./components/Footer";
+import { useRef, RefObject } from "react";
+
+interface AppProps {
+  onAboutClick: () => void;
+  onSkillsClick: () => void;
+  onProjectsClick: () => void;
+  onContactClick: () => void;
+}
 
 function App() {
+  const aboutRef: RefObject<HTMLDivElement> = useRef(null);
+  const skillsRef: RefObject<HTMLDivElement> = useRef(null);
+  const projectsRef: RefObject<HTMLDivElement> = useRef(null);
+  const contactRef: RefObject<HTMLDivElement> = useRef(null);
+
+  const scrollToSection = (ref: RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="App">
-      <Header />
+      <Header
+        onAboutClick={() => scrollToSection(aboutRef)}
+        onSkillsClick={() => scrollToSection(skillsRef)}
+        onProjectsClick={() => scrollToSection(projectsRef)}
+        onContactClick={() => scrollToSection(contactRef)}
+      />
       <Introduce />
-      <About />
-      <Skills />
-      <MyProjects />
+      <div ref={aboutRef}>
+        <About />
+      </div>
+      <div ref={skillsRef}>
+        <Skills />
+      </div>
+      <div ref={projectsRef}>
+        <MyProjects />
+      </div>
+      <div ref={contactRef}>
+        <Contact />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
