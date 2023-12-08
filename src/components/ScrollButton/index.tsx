9557ffc52e5c4fb7b3a7b3dfa5748d44b73f88ba
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-12-08 15:23:58 +0300

    Create index.tsx

---
+++ src/components/ScrollButton/index.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { FaArrowCircleUp } from "react-icons/fa";
+import { Button } from "@mui/material";
+import "./styles.css";
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
+
+  return (
+    <Button className="scroll-button">
+      <FaArrowCircleUp
+        onClick={scrollToTop}
+        style={{ display: visible ? "inline" : "none" }}
+      />
+    </Button>
+  );
+};
+
+export default ScrollButton;
