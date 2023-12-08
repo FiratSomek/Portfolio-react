2e34ac0ed96f48f43485e2252a3a2cf472002429
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-12-08 15:23:35 +0300

    Update index.tsx

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,17 +1,18 @@
+import { Typography } from "@mui/material";
 import "./styles.css";
 
 export const About = () => {
   return (
-    <div className="about-container">
+    <div className="about-container ">
       <div>
-        <h2>About Me</h2>
+        <Typography variant="h4">About Me</Typography>
       </div>
       <div className="about-info">
         {/* <div>
           <img src={ImageName} alt="#" />
         </div> */}
         <div>
-          <p>
+          <Typography variant="body1">
             My name is Fırat and I am a talented front end developer. Actually ı
             have been graduated from aviation management department. But ı have
             always been interest with internet and technology. In my free time,
@@ -20,7 +21,7 @@ export const About = () => {
             quickly realized that it was something I was truly passionate about
             and decided to pursue a career change. I am so excited to will gain
             new experiences and help others in the process.
-          </p>
+          </Typography>
         </div>
       </div>
     </div>
