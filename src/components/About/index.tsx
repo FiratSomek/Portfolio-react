45c5c36eb5931c9d29739b9e97dc87cc34d5a297
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-12-11 11:51:05 +0300

    Update index.tsx

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,21 +8,16 @@ export const About = () => {
         <Typography variant="h4">About Me</Typography>
       </div>
       <div className="about-info">
-        {/* <div>
-          <img src={ImageName} alt="#" />
-        </div> */}
-        <div>
-          <Typography variant="body1">
-            My name is Fırat and I am a talented front end developer. Actually ı
-            have been graduated from aviation management department. But ı have
-            always been interest with internet and technology. In my free time,
-            I enjoy listening to music and hiking, that it helps to relax and
-            inspire me in my work. I initially started coding as a hobby, but
-            quickly realized that it was something I was truly passionate about
-            and decided to pursue a career change. I am so excited to will gain
-            new experiences and help others in the process.
-          </Typography>
-        </div>
+        <Typography variant="body1">
+          My name is Fırat and I am a talented front end developer. Actually ı
+          have been graduated from aviation management department. But ı have
+          always been interest with internet and technology. In my free time, I
+          enjoy listening to music and hiking, that it helps to relax and
+          inspire me in my work. I initially started coding as a hobby, but
+          quickly realized that it was something I was truly passionate about
+          and decided to pursue a career change. I am so excited to will gain
+          new experiences and help others in the process.
+        </Typography>
       </div>
     </div>
   );
